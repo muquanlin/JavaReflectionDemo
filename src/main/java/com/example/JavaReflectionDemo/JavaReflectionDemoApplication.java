d08853a00132e29cf5f5895272d59c9ff62cb19d
quanlin.mu <quanlin.mu@ecarx.com.cn>
2020-04-27 16:17:31 +0800

    初始化项目

---
+++ src/main/java/com/example/JavaReflectionDemo/JavaReflectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaReflectionDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReflectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReflectionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaReflectionDemo/JavaReflectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaReflectionDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReflectionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
